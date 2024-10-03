ca3558c41f0feaf23f68cde95e7433aeb589e804
priyanshu45sahu <priyanshusahu.9047@gmail.com>
2024-10-03 20:28:32 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,9 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
+app.get("/", (req, res) => {
+  res.json("This is ecom server")
+})
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
