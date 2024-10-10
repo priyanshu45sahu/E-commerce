5768e6bfc098a6a49506e44ec29047b3a849adc5
priyanshu45sahu <priyanshusahu.9047@gmail.com>
2024-10-11 01:39:41 +0530

    update backend url

--- client/src/hooks/useCategory.js
+++ client/src/hooks/useCategory.js
@@ -8,7 +8,7 @@ export default function useCategory() {
   //get cat
   const getCategories = async () => {
     try {
-      const { data } = await axios.get("${API_URL}/api/v1/category/get-category");
+      const { data } = await axios.get(`${API_URL}/api/v1/category/get-category`);
       setCategories(data?.category);
     } catch (error) {
       console.log(error);
